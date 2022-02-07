c1af4137db8dfaa02248cc8eac3d27242cb4520f
Madni Pothiyawala <madnipothiyawala92@gmail.com>
2022-02-07 16:00:35 +0530

    Update program.cs

--- WiredBrainCoffeeShopTool/Program.cs
+++ WiredBrainCoffeeShopTool/Program.cs
@@ -8,7 +8,7 @@ namespace WiredBrainCoffeeShopTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee Shop");
-            Console.WriteLine("Write help to display available commands");
+            Console.WriteLine("Write help to display available coffee shop commands");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
